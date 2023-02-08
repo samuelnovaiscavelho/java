a16abf241029c959c69ebed023eead4f68b1c291
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-02-08 16:57:34 -0300

    Correção Exercicio 4

--- variaveisoperadores/src/variaveisoperadores/CalculoDiferencaEx4.java
+++ variaveisoperadores/src/variaveisoperadores/CalculoDiferencaEx4.java
@@ -7,19 +7,19 @@ public class CalculoDiferencaEx4 {
 	public static void main(String[] args) {
 		
 		Scanner leia = new Scanner(System.in);
-		int n1, n2, n3, n4, diferenca;
+		float n1, n2, n3, n4, diferenca;
 		
 		System.out.println("numero1: ");
-		n1 = leia.nextInt();
+		n1 = leia.nextFloat();
 		
 		System.out.println("numero2: ");
-		n2 = leia.nextInt();
+		n2 = leia.nextFloat();
 		
 		System.out.println("numero3: ");
-		n3 = leia.nextInt();
+		n3 = leia.nextFloat();
 		
 		System.out.println("numero4: ");
-		n4 = leia.nextInt();
+		n4 = leia.nextFloat();
 		
 		diferenca = (n1 * n2) - (n3 * n4);
 		
